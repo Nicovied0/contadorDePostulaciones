723786f9f460c0960cb0c3f5fb782039eabb4531
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-05-22 20:50:53 -0300

    aplication contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contadorDePostulaciones';
+  public postulacionesHoy: number = 0;
+  public postulacionLogradas: boolean = false;
+  public postulacionesEnLaSemana: number = 0;
+  public contadorLocal: number = 0;
+  public aux: string | null = localStorage.getItem('postulaciones');
+  public aux2: number = 0;
+
+  constructor() {
+    if (this.aux) {
+      this.contadorLocal = parseInt(this.aux);
+    }
+    this.postulacionesHoy = 0;
+  }
+
+  sumar() {
+    this.postulacionesHoy += 1;
+    this.aux2 = parseInt(this.aux ?? '0');
+    this.contadorLocal = this.aux2 + this.postulacionesHoy;
+    localStorage.setItem('postulaciones', this.contadorLocal.toString());
+    if (this.postulacionesHoy >= 25) {
+      this.postulacionLogradas = true;
+    }
+  }
+
+  restar() {
+    this.postulacionesHoy -= 1;
+    this.aux2 = parseInt(this.aux ?? '0');
+    this.contadorLocal = this.aux2 + this.postulacionesHoy;
+    localStorage.setItem('postulaciones', this.contadorLocal.toString());
+    if (this.postulacionesHoy >= 25) {
+      this.postulacionLogradas = true;
+    }
+  }
+
+  clear() {
+    localStorage.removeItem('postulaciones');
+    location.reload();
+  }
 }
